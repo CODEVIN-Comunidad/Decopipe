f8a603200e893fef586db829c7aeb32b662f15f3
alabariciaz <47753271+alabariciaz@users.noreply.github.com>
2020-02-01 15:55:11 -0600

    Main Scene & Level 1
    Prefaps, Tilemaps, Fonts, Resours and Scripts were added to the project.
    In the "Main Scene" the prefap "canvas" was added and in the "Level 1" scene the prefap "canvas 1" and "grid" were added.

---
+++ Assets/Scripts/MoveGround.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveGround : MonoBehaviour
+{
+    [Range(-30.0f, 0.0f)]
+    public float moveSpeed =-5f;
+    public Grid tilemap;
+   
+    private float finalSpeed;
+    private int state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = 0;
+    }
+
+    // Update is called once per frame
+   void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            state = 1;
+        }
+
+        if (state == 1)
+        {
+            ParallaxMoveGround();
+        }
+    }
+
+    private void ParallaxMoveGround()
+    {
+        finalSpeed = moveSpeed * Time.deltaTime;
+        tilemap.transform.position = new Vector3(tilemap.transform.position.x + finalSpeed, tilemap.transform.position.y);
+    }
+}
---
+++ Assets/Scripts/ParalaxController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParalaxController : MonoBehaviour
+{
+    [Range(0f, 0.20f)]
+    public float parallaxSpeed = 0.02f;
+    public RawImage background1;
+
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        float finalSpeed = parallaxSpeed * Time.deltaTime;
+        background1.uvRect = new Rect(background1.uvRect.x + finalSpeed, background1.uvRect.y + finalSpeed, 1f, 1f); 
+    }
+}
