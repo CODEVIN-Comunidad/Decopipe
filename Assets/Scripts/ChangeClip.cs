d2a50efeafd3a5eba28606a1b1b28b30d16354e0
Arquery <sgu_sigu@msn.com>
2020-02-01 17:18:27 -0700

    Music and MainScene
    The music and music effects were added in the project, also was created a scrpit for controlling audio in the Main Scene.

---
+++ Assets/Scripts/ChangeClip.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*
+ * This script is used to change the clip of audio source of the MainScene
+ */
+
+
+public class ChangeClip : MonoBehaviour
+{
+
+    private GameObject canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = GameObject.Find("Main Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!canvas.GetComponent<AudioSource>().isPlaying)
+        {
+            SceneManager.LoadScene("Level 1");
+        }
+    }
+
+    //This method change the audioclip after push a button
+    public void ChangeAudio()
+    {
+        canvas.GetComponent<AudioSource>().clip = Resources.Load<AudioClip>("Music/Hit trompetas");
+        canvas.GetComponent<AudioSource>().Play();
+        canvas.GetComponent<AudioSource>().loop = false;
+    }
+}
