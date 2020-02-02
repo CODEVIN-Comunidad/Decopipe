1632211db1cd96362de81222df220f8f17b4abd9
Arquery <sgu_sigu@msn.com>
2020-02-01 21:56:17 -0700

    Character Gameplay
    The scripts, buttons and modifieds, were added to project.

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+
+    public float jumpForce;
+    private Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        jumpForce = TriggerEvent.jump;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Jumping()
+    {
+        rigidbody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        /*if (Input.GetButtonDown("YellowButton"))
+        {
+            rigidbody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        }
+        if (Input.GetButtonDown("BrownButton"))
+        {
+            rigidbody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        }
+        if (Input.GetButtonDown("RedButton"))
+        {
+            rigidbody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        }
+        if (Input.GetButtonDown("BlueButton"))
+        {
+            rigidbody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        }*/
+    }
+}
---
+++ Assets/Scripts/TriggerEvent.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriggerEvent : MonoBehaviour
+{
+    private GameObject yellow;
+    private GameObject brown;
+    private GameObject red;
+    private GameObject blue;
+    private AudioSource jumpTrumpet;
+
+    public static float jump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        yellow = GameObject.Find("YellowButton");
+        brown = GameObject.Find("BrownButton");
+        red = GameObject.Find("RedButton");
+        blue = GameObject.Find("BlueButton");
+        jumpTrumpet = GameObject.Find("Grid").GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TouchYellow()
+    {
+        jump = 5;
+        OnCollisionEnter2D();
+        jumpTrumpet.clip = Resources.Load<AudioClip>("Music/Botón 2");
+        jumpTrumpet.Play();
+    }
+
+    public void TouchBrown()
+    {
+        jump = 10;
+        OnCollisionEnter2D();
+        jumpTrumpet.clip = Resources.Load<AudioClip>("Music/Botón 4");
+        jumpTrumpet.Play();
+    }
+
+    public void TouchRed()
+    {
+        jump = 8;
+        OnCollisionEnter2D();
+        jumpTrumpet.clip = Resources.Load<AudioClip>("Music/Botón 1");
+        jumpTrumpet.Play();
+    }
+
+    public void TouchBlue()
+    {
+        jump = 6;
+        OnCollisionEnter2D();
+        jumpTrumpet.clip = Resources.Load<AudioClip>("Music/Botón 3");
+        jumpTrumpet.Play();
+    }
+
+    private void OnCollisionEnter2D()
+    {
+        yellow.GetComponent<Button>().enabled = false;
+        brown.GetComponent<Button>().enabled = false;
+        red.GetComponent<Button>().enabled = false;
+        blue.GetComponent<Button>().enabled = false;
+
+        yellow.GetComponent<Image>().enabled = false;
+        brown.GetComponent<Image>().enabled = false;
+        red.GetComponent<Image>().enabled = false;
+        blue.GetComponent<Image>().enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Flag")
+        {
+            Debug.Log("Ouch");
+            yellow.GetComponent<Button>().enabled = true;
+            brown.GetComponent<Button>().enabled = true;
+            red.GetComponent<Button>().enabled = true;
+            blue.GetComponent<Button>().enabled = true;
+
+            yellow.GetComponent<Image>().enabled = true;
+            brown.GetComponent<Image>().enabled = true;
+            red.GetComponent<Image>().enabled = true;
+            blue.GetComponent<Image>().enabled = true;
+
+        }
+    }
+}
